0ab3817b838ac5edf4881300baa30e632fb8b557
Danilo Machado <danilo.aquino@coopersystem.com.br>
2022-01-10 09:10:37 -0300

    tests: check benchmark;

--- cryptocurrency/cryptocurrency_benchmark.go
+++ cryptocurrency/cryptocurrency_benchmark.go
@@ -5,14 +5,29 @@ import (
 	"os"
 	"testing"
 
+	"github.com/xxdannilinxx/klv/db"
 	ccpb "github.com/xxdannilinxx/klv/proto/gen/ccpb"
+	"github.com/xxdannilinxx/klv/utils"
 	"golang.org/x/net/context"
+)
+
+var (
+	config utils.Config = utils.Config{
+		PORT:              os.Getenv("PORT"),
+		POSTGRES_USER:     os.Getenv("POSTGRES_USER"),
+		POSTGRES_PASSWORD: os.Getenv("POSTGRES_PASSWORD"),
+		POSTGRES_DB:       os.Getenv("POSTGRES_DB"),
+		POSTGRES_HOST:     os.Getenv("POSTGRES_HOST"),
+		POSTGRES_PORT:     os.Getenv("POSTGRES_PORT"),
+	}
 )
 
 func BenchmarkGetMostVotedCryptoCurrency(b *testing.B) {
 	l := log.New(os.Stdout, "klv-api-benchmark - ", log.LstdFlags)
 
-	s := NewCryptoCurrency(l, Repository)
+	dbConn := db.ConnectDB(config)
+	repository := &CryptoCurrencyRepository{dbConn}
+	s := NewCryptoCurrency(l, repository)
 
 	for i := 0; i < b.N; i++ {
 		s.GetMostVotedCryptoCurrency(context.Background(), &ccpb.GetMostVotedCryptoCurrencyRequest{})
